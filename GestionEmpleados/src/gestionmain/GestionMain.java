6feba5b5270e121f260689bb49ce7418a9efaa05
Javt-0 <jonathan96aguilar@gmail.com>
2022-10-03 11:45:14 +0200

    hecho

--- GestionEmpleados/src/gestionmain/GestionMain.java
+++ GestionEmpleados/src/gestionmain/GestionMain.java
@@ -10,7 +10,6 @@ public class GestionMain {
     public static int cont;
     public static void main(String[] args) {
         Persona p1 = new Persona("Jonathan", "Aguilar");
-        Persona p2 = new Persona("Ingrid", "Ayala");
         System.out.println(p1);
     }
 }
